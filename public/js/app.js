c9b4cf1113425cdd14d54e0a316e912c88e62859
qaisw96 <qaisw14@gmail.com>
2021-04-06 22:05:27 +0300

    update/delete

---
+++ public/js/app.js
@@ -0,0 +1,9 @@
+function showBottom() {
+    const x = document.getElementById('show')
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  
+}
